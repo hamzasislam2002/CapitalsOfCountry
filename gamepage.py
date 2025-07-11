4ff11566ce8cce50ff39150a9c53bf1511bd8c28
Hamza Islam <islam.144@osu.edu>
2025-07-11 18:20:16 -0400

    Adding New Conditions

--- gamepage.py
+++ gamepage.py
@@ -1,4 +1,4 @@
-import northamerica, random
+import northamerica, random, southamerica
 score = 0
 
 print("                           Welcome to Country\'s Capitals                      ")
@@ -13,16 +13,52 @@ print("NOTE: Any number less than 1 or greater than 6 will also quit the game as
 
 answer = int(input("Which continent do you want to explore: "))
 na_list = []
+sa_list = []
 for x in northamerica.north_america_capitals.keys():
     na_list.append(x)
-
+for y in southamerica.south_america_capitals.keys():
+    sa_list.append(y)
 print(na_list)
-random_answer = random.choice(na_list)
-answerz = input(f"What is the capital of {random_answer}")
-if answerz == northamerica.north_america_capitals[random_answer]:
-    print("YAY")
+print(sa_list)
+
+if answer == 1:
+    random_answer_na = random.choice(na_list)
+    answer_na = input(f"What is the capital of {random_answer_na}: ")
+    if answer_na == northamerica.north_america_capitals[random_answer_na]:
+        print("YAY")
+    else:
+        print("NAY")
+elif answer == 2:
+    random_answer_sa = random.choice(sa_list)
+    answer_sa = input(f"What is the capital of {random_answer_sa}: ")
+    if answer_sa == southamerica.south_america_capitals[random_answer_sa]:
+        print("YAY")
+    else:
+        print("NAY")
+elif answer == 3:
+    random_answer_sa = random.choice(sa_list)
+    answer_sa = input(f"What is the capital of {random_answer_sa}: ")
+    if answer_sa == southamerica.south_america_capitals[random_answer_sa]:
+        print("YAY")
+    else:
+        print("NAY")
+elif answer == 4:
+    random_answer_sa = random.choice(sa_list)
+    answer_sa = input(f"What is the capital of {random_answer_sa}: ")
+    if answer_sa == southamerica.south_america_capitals[random_answer_sa]:
+        print("YAY")
+    else:
+        print("NAY")
+elif answer == 5:
+    random_answer_af = random.choice(af_list)
+    answer_af = input(f"What is the capital of {random_answer_af}: ")
+    if answer_af == southamerica.south_america_capitals[random_answer_af]:
+        print("YAY")
+    else:
+        print("NAY")
 else:
-    print("NAY")
+    print("GAME OVER")
+
 
 
 
---
+++ southamerica.py
@@ -0,0 +1,14 @@
+south_america_capitals = {
+    "Argentina":"Buenos Aires",
+    "Bolivia":"La Paz & Sucre",
+    "Brazil":"Brasilia",
+    "Chile":"Santiago",
+    "Colombia":"Bogota",
+    "Ecuador":"Quito",
+    "Guyana":"Georgetown",
+    "Paraguay":"Asunción",
+    "Peru":"Lima",
+    "Suriname":"Paramaribo",
+    "Uruguay":"Montevideo",
+    "Venezuela":"Caracas"
+}
